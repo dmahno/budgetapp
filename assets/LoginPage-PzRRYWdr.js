ee345a47ca0554b400ee872d226407325616601b
dmitry <flik.88@mail.ru>
2024-10-29 02:48:19 +0300

    Updates

--- assets/LoginPage-PzRRYWdr.js
+++
@@ -1,1 +0,0 @@
-import{o,j as r}from"./index-DMvfeoQm.js";import{A as t}from"./SignUp-Bew85udb.js";const i=o(()=>r.jsx(t,{}));export{i as LoginPage};
--- assets/NotFoundPage-m_QrfAGj.js
+++
@@ -1,1 +0,0 @@
-import{o as r,j as o}from"./index-DMvfeoQm.js";const t=r(()=>o.jsx(o.Fragment,{children:"NotFoundPage"}));export{t as NotFoundPage};
